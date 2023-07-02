a52e6d9516e440ba089e703a60b13cb80a079eb9
xuzhougeng <xuzhougeng@yeah.net>
2023-07-02 23:28:50 +0800

    remove unnecessary files

--- build/lib/pywolai/enums/__init__.py
+++
@@ -1,9 +0,0 @@
-from pywolai.enums.block_align import BlockAlign
-from pywolai.enums.block_back_colors import BlockBackColors
-from pywolai.enums.block_front_colors import BlockFrontColors
-from pywolai.enums.block_type import BlockTypes
-from pywolai.enums.inline_title_type import InlineTitleType
-from pywolai.enums.text_align import TextAlign
-from pywolai.enums.rich_text import RichText
-
-from pywolai.enums.error_code import ErrorCode
--- build/lib/pywolai/enums/block_align.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class BlockAlign(Enum):
-    LEFT = "left" # "左对齐"
-    CENTER = "center" # "居中"
-    RIGHT = "right" # "右对齐"
