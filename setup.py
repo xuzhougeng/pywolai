2c3ff8919649d0b0b5d2393caf7cc0b82b299b38
xuzhougeng <xuzhougeng@yeah.net>
2023-07-03 10:05:34 +0800

    modify for upload to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8' ) as f:
+    long_description = f.read()
+
+
 setup(
     name='pywolai',
     version='0.1.0',
-    author='Xu Zhou-Geng',
-    author_email='xuzhougeng',
-    description='A brief description of your project.',
+    author='xuzhougeng',
+    author_email='xuzhougeng@163.com',
+    description='Python wrapper for wolai API',
     packages=find_packages(),
     install_requires=[
         'requests',  
     ],
 )
+
